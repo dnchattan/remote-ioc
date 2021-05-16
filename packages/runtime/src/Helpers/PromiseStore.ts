9667990a4fec71f5b309879c2a7159f9ccf75723
Daniel Chattan <dnchattan@gmail.com>
2021-05-16 17:49:26 -0400

    add TODO to PromiseStore

--- packages/runtime/src/Helpers/PromiseStore.ts
+++ packages/runtime/src/Helpers/PromiseStore.ts
@@ -7,6 +7,10 @@ export class PromiseStore {
   private promiseIdx = 0;
   private readonly promises = new Map<number, PromiseCallbacks>();
 
+  // TODO Add a timeout value and automatically expire stale promises
+  // must use setTimeout and let it go dormant when map is empty to avoid
+  // long running interval keeping the process alive
+
   create(): [number, Promise<any>] {
     let callbacks!: PromiseCallbacks;
     const promise = new Promise<any>((resolve, reject) => {
