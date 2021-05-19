b01a5517aadf21e7f99516c34f751f4625a731e2
Daniel Chattan <dnchattan@gmail.com>
2021-05-19 08:01:43 -0400

    add 'off' to non-promise keys in Promisify

--- packages/runtime/src/Types/PromiseTypes.ts
+++ packages/runtime/src/Types/PromiseTypes.ts
@@ -1,6 +1,6 @@
 export type EnsurePromise<T> = T extends Promise<any> ? T : Promise<T>;
 export type Promisify<T> = {
-  [key in keyof T]: key extends 'on'
+  [key in keyof T]: key extends 'on' | 'off'
     ? T[key]
     : T[key] extends (...args: infer P) => infer R
     ? (...args: P) => EnsurePromise<R>
